28d8d02f66a92bccd58c40ed0b12d18b3591c977
Rocket2510 <rocket251019@gmail.com>
2023-01-25 10:53:24 -0600

    Boton iniciar Sesion

--- app/src/main/java/com/example/quiz_app_equipoa/MainActivity.java
+++ app/src/main/java/com/example/quiz_app_equipoa/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.quiz_app_equipoa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
         //Comentario en el main activity.
         //Otro comentario.
 
+    }
+
+    //Metodo para acceder a la pantalla de inicio (En lo que programamos la validacion)
+    public void IniciarSesion (View v){
+        Intent i = new Intent(this, Pantalla_Inicio.class);
+        startActivity(i);
     }
 }
