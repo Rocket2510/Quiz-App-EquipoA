29289bb35cb6a7756357bbd23755e630529eaed2
Rocket2510 <rocket251019@gmail.com>
2023-01-25 19:08:33 -0600

    Modulo1

---
+++ app/src/main/java/com/example/quiz_app_equipoa/Modulo1/Pregunta_1.java
@@ -0,0 +1,16 @@
+package com.example.quiz_app_equipoa.Modulo1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.quiz_app_equipoa.R;
+
+public class Pregunta_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pregunta1_m1);
+    }
+}
